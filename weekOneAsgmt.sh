36c3fd1f044a1d4aae8dd94099745a997514bfaf
Steve Roma <sjroma@hotmail.com>
2017-06-02 12:08:55 -0400

    check in code for week 1 assignment

---
+++ weekOneAsgmt.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir week-one-project
+cd week-one-project
+touch index.html styles.css README.md
+mkdir images about newsletter
+echo "#Week One Project\n- HTML\n- CSS" > README.md
+mv ~/Downloads/optimized/*.* images
